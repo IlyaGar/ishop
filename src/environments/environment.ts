5eb19ba6df22fa24dadd337d4078325e6bef7a21
ilyaGar <ilya.sauko@gmail.com>
2020-09-23 14:45:30 +0300

    Return of products element.status === 'завершен')

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrlLog: 'http://192.168.1.38:120',
-  apiUrl: 'http://192.168.1.38:120',
+  apiUrlLog: 'http://192.168.1.232:120',
+  apiUrl: 'http://192.168.1.232:120',
   apiUrlImg: 'https://mile.by/gtools/getImg/index.php',
   cookieName: 'user-ishope-mile',
   listAdminsIshop: ['petrovich_a', 'maksimovich_v', 'shegolov_a', 'orlova_v', 'gurinenko_n', 'Sauko_i'],
